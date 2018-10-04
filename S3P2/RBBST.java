513da8b561ce0d13347f670ce5d26dd3aeebcc85
yue-qiu <canbaodetudou@gmail.com>
2018-10-04 16:01:27 +0800

    实现红黑树delete方法

--- S3P2/RBBST.java
+++ S3P2/RBBST.java
@@ -1,6 +1,5 @@
 package Algorithms.S3P2;
 
-import java.security.Key;
 
 public class RBBST<Key extends Comparable<Key>, Value> {
     private Node root;
@@ -24,6 +23,25 @@ public class RBBST<Key extends Comparable<Key>, Value> {
         }
     }
 
+    public Value get(Key key)
+    {
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key)
+    {
+        if (x == null)
+            return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0)
+            get(x.left, key);
+        else if (cmp > 0)
+            get(x.right, key);
+        else
+            return x.value;
+        return null;
+    }
+
     public int size()
     {
         return size(root);
@@ -31,6 +49,8 @@ public class RBBST<Key extends Comparable<Key>, Value> {
 
     private int size(Node x)
     {
+        if (x == null)
+            return 0;
         return x.N;
     }
 
@@ -44,6 +64,12 @@ public class RBBST<Key extends Comparable<Key>, Value> {
     {
         if (x == null)
             return new Node(key, value, RED, 1);
+        /*
+        将这段代码放到这可以效率，因为这样每次只对几个结点进行操作
+        这样就是 2-3-4 树
+        if (isRED(x.left) && isRED(x.right))
+            filpColors(x);
+        * */
         int cmp = key.compareTo(x.key);
         if (cmp < 0)
             x.left = put(x.left, key, value);
@@ -52,11 +78,14 @@ public class RBBST<Key extends Comparable<Key>, Value> {
         else
             x.value = value;
 
-        if (isRed(x.right) && !isRed(x.left))
+        // 调整树的结构，修正右倾、分解 4-结点
+
+        if (isRed(x.right) && !isRed(x.left)) // 红链接在右
             x = rotateLeft(x);
-        if (isRed(x.left) && isRed(x.left.left))
+        if (isRed(x.left) && isRed(x.left.left)) // 两个红链接相连
             x = rotateRight(x);
-        if (isRed(x.left) && isRed(x.right))
+        // 切分 4-node，由于每次在最后都将临时构造的 4-node 结点切分，也就变成了 2-3 树
+        if (isRed(x.left) && isRed(x.right)) // 左右链接均为红链接，即一个 4-node 结点
             filpColors(x);
 
         x.N = 1 + size(x.left) + size(x.right);
@@ -101,8 +130,144 @@ public class RBBST<Key extends Comparable<Key>, Value> {
 
     private void filpColors(Node h)
     {
-        h.color = RED;
-        h.left.color = BLACK;
-        h.right.color = BLACK;
+        // 分解 4- 结点，将中间结点变红送入父结点
+        h.color = !h.color;
+        h.left.color = !h.left.color;
+        h.right.color = !h.right.color;
+    }
+
+    public void deleteMin()
+    {
+        root = deleteMin(root);
+        root.color = BLACK;
+    }
+
+    private Node deleteMin(Node h)
+    {
+        if (h.left == null)
+            return null;
+        // 如果 h.left 与 h.left.left 都是黑，说明 h.left 是一个 2-结点
+        // 为了防止删除 2-结点导致红黑树平衡被破坏，需要借结点与 h.left 组成 3-结点或 4-结点
+        if (!isRed(h.left) && !isRed(h.left.left))
+            h = moveRedLeft(h);
+        h.left = deleteMin(h.left);
+        return balacne(h);
+    }
+
+    private Node moveRedLeft(Node h)
+    {
+        // 将 h、h.left、h.right 结合成一个 4-结点
+        filpColors(h);
+        // 如果 h.right本来不是一个 2-结点，显然不能直接与 h、h.left结合
+        // 转为从 h.right借一个结点与 h.left 组成一个 3-结点
+        if (isRed(h.right.left))
+        {
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+            filpColors(h);
+        }
+        return h;
+    }
+
+    public void deleteMax()
+    {
+        root = deleteMax(root);
+        root.color = BLACK;
+    }
+
+    private Node deleteMax(Node h)
+    {
+        // 左倾红黑树红链接都在左边，为了找到最大值将红链接移到右边
+        if (isRed(h.left))
+            h = rotateRight(h);
+        if (h == null)
+            return null;
+        // 如果 h.right 和 h.right.left 均为黑，说明 h.right 是 2-结点，需要借结点
+        // (因为是左倾红黑树，通过 h.right.left 是否为红判断 h.right 是否与下层结点组成 3-结点)
+        if (!isRed(h.right) && !isRed(h.right.left))
+            h = moveRedRight(h);
+        h.right = deleteMax(h.right);
+        return balacne(h);
+    }
+
+    private Node moveRedRight(Node h)
+    {
+        // 将 h、h.left、h.right 结合成一个 4-结点
+        filpColors(h);
+        // 如果 h.left.left 为红，说明 h.left 本身不是一个 2-结点，不能直接与 h.right、h结合
+        // 从 h.left 借结点与 h.right 组成 3-结点
+        if (isRed(h.left.left))
+        {
+            h = rotateRight(h);
+            filpColors(h);
+        }
+        return h;
+    }
+
+    public void delete(Key key)
+    {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node h, Key key)
+    {
+        int cmp = key.compareTo(h.key);
+        // key 在左侧
+        if (cmp < 0)
+        {
+            // 如果 h.left 是一个 2-结点，借结点
+            if (!isRed(h.left) && !isRed(h.left.left))
+                h = moveRedLeft(h);
+            // 递归删除
+            h.left = delete(h.left, key);
+        }
+        // key 在右侧或 h.key == key
+        else
+        {
+            // 将红链接转到右侧
+            if (isRed(h.left))
+                h = rotateRight(h);
+            // 在树的底部相等，直接删除
+            if (cmp == 0 && (h.right == null))
+                return null;
+            // h.right 不是 2-node，需要借结点
+            if (!isRed(h.right) && !isRed(h.right.left))
+                h = moveRedRight(h);
+            // 不在树底相等，用继承结点覆盖 h 并将其从右子树中删除
+            if (cmp == 0)
+            {
+                h.key = min(h.right);
+                h.value = get(h.key);
+                h.right = deleteMin(h.right);
+            }
+            else
+                h.right = delete(h.right, key);
+        }
+        return balacne(h);
+    }
+
+    /**
+     * 修正右倾链接，分解 4-结点
+     * @param x 结点
+     * @return 左倾、非 4-结点的节点
+     */
+    private Node  balacne(Node x)
+    {
+        if (isRed(x.right))
+            x = rotateLeft(x);
+        if (isRed(x.left) && isRed(x.left.left))
+            x = rotateRight(x);
+        if (isRed(x.left) && isRed(x.right))
+            filpColors(x);
+
+        x.N = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    private Key min(Node h)
+    {
+        if (h.left == null)
+            return h.key;
+        return min(h.left);
     }
 }
