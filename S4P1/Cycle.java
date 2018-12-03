79154a4402885e353e6e00339b6188d6fb298be5
yue-qiu <canbaodetudou@gmail.com>
2018-12-03 14:25:54 +0800

    判断无环图

--- S4P1/Cycle.java
+++ S4P1/Cycle.java
@@ -1,4 +1,34 @@
 package Algorithms.S4P1;
 
+/**
+ * 判断是否为无环图，
+ */
 public class Cycle {
+    private boolean[] marked;
+    private boolean hasCycle = false;
+
+    public Cycle(Graph G)
+    {
+        marked = new boolean[G.getV()];
+        for (int i = 0; i < G.getV(); i++)
+            if (!marked[i])
+                dfs(G, i, i);
+    }
+
+    private void dfs(Graph G, int v, int u)
+    {
+        marked[v] = true;
+        for (int w: G.adj(v))
+        {
+            if (!marked[w])
+                dfs(G, w, v);
+            else if (w != u)
+                hasCycle = true;
+        }
+    }
+
+    public boolean hasCycle()
+    {
+        return hasCycle;
+    }
 }
