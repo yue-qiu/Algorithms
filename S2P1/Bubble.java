5989803967c77959bd2f4a29e76f113a507c5b56
qiuyue <qiuyue9971@126.com>
2019-01-16 18:50:40 +0800

    精简算法

--- S2P1/Bubble.java
+++ S2P1/Bubble.java
@@ -18,22 +18,33 @@ public class Bubble extends Example{
      * 每趟排序设置一个flag，如果一趟排序下来发现没有发生交换则认为这个数组已经是有序的，无需继续比较了
      * @param a 待排序数组
      */
+//    public static void flagSort(Comparable[] a)
+////    {
+////        boolean flag = true;
+////        int len = a.length;
+////        while (flag)
+////        {
+////            flag = false;
+////            for (int j = 1; j < len; j++)
+////            {
+////                if (less(a[j], a[j-1]))
+////                {
+////                    exch(a, j, j-1);
+////                    flag = true;
+////                }
+////            }
+////            len--;
+////        }
+////    }
     public static void flagSort(Comparable[] a)
     {
-        boolean flag = true;
         int len = a.length;
-        while (flag)
-        {
-            flag = false;
+        for (boolean flag = false; flag = !flag; len--)
             for (int j = 1; j < len; j++)
-            {
                 if (less(a[j], a[j-1]))
                 {
                     exch(a, j, j-1);
-                    flag = true;
+                    flag = false;
                 }
-            }
-            len--;
-        }
     }
 }
